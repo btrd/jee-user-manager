0cdc71163c0c115e9f6b58acfc91843e36bcb76f
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-01-03 22:48:55 +0100

    Add UserService

---
+++ src/main/java/fr/eservices/ifi/user_manager/srv/UserService.java
@@ -0,0 +1,25 @@
+package fr.eservices.ifi.user_manager.srv;
+
+import java.util.List;
+
+import fr.eservices.ifi.user_manager.entity.User;
+
+public interface ResellerService {
+  
+  // Pass SESSION/COOKIES/SOMETHING in params
+  public User getAuthenticatedUser();
+
+  public String getLoginPageUrl();
+
+  public void create(User user);
+
+  public User update(Long userId, String firstname, String lastname, String email, String password, String role);
+
+  public boolean delete(Long userId);
+
+  public List<User> list();
+
+  public List<User> listByLastName(String lastname);
+
+  public List<User> listByRole(String role);
+}
