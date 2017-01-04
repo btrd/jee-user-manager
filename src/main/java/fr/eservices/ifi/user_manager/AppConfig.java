83c6839036e04768da9e135eadd54ba666e753e2
Alexandre Thorez <alexandrethorez@laposte.net>
2017-01-04 03:01:10 +0100

    change to spring mvc application context

---
+++ src/main/java/fr/eservices/ifi/user_manager/AppConfig.java
@@ -0,0 +1,22 @@
+package fr.eservices.ifi.user_manager;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("fr.eservices.ifi")
+@EnableWebMvc
+public class AppConfig {
+
+	@Bean
+	public InternalResourceViewResolver jspViewResolver() {
+		InternalResourceViewResolver bean = new InternalResourceViewResolver();
+		bean.setPrefix("/WEB-INF/views/");
+		bean.setSuffix(".jsp");
+		return bean;
+	}
+
+}
