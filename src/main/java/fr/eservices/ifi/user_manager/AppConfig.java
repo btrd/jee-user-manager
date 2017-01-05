bc209138af870a294e9cb1370fbf74e303d43ddb
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-01-05 11:07:46 +0100

    Fix connection to DB

--- src/main/java/fr/eservices/ifi/user_manager/AppConfig.java
+++ src/main/java/fr/eservices/ifi/user_manager/AppConfig.java
@@ -1,22 +1,84 @@
 package fr.eservices.ifi.user_manager;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-@ComponentScan("fr.eservices.ifi")
 @EnableWebMvc
+@ComponentScan({
+  "fr.eservices.ifi.user_manager"
+})
+@EnableTransactionManagement
 public class AppConfig {
+  
+  /*
+  @Bean
+  public InternalResourceViewResolver getViewResolver() {
+    InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+    resolver.setPrefix("META-INF/resources/WEB-INF/views/");
+    resolver.setSuffix(".jsp");
+    return resolver;
+  }
+  */
+  @Bean
+  public InternalResourceViewResolver jspViewResolver() {
+    InternalResourceViewResolver bean = new InternalResourceViewResolver();
+    bean.setPrefix("/WEB-INF/views/");
+    bean.setSuffix(".jsp");
+    return bean;
+  }
 
-	@Bean
-	public InternalResourceViewResolver jspViewResolver() {
-		InternalResourceViewResolver bean = new InternalResourceViewResolver();
-		bean.setPrefix("/WEB-INF/views/");
-		bean.setSuffix(".jsp");
-		return bean;
-	}
+  @Bean
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+    em.setDataSource(dataSource());
+    em.setPackagesToScan(new String[] { 
+      "fr.eservices.ifi.user_manager.entity" 
+    });
+
+    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    em.setJpaVendorAdapter(vendorAdapter);
+
+    Properties prop = new Properties();
+    prop.setProperty("hibernate.show_sql", "true");
+    prop.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+    prop.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+    em.setJpaProperties(prop);
+
+    return em;
+  }
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.h2.Driver");
+    dataSource.setUrl("jdbc:h2:./paraDb");
+    return dataSource;
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+    JpaTransactionManager transactionManager = new JpaTransactionManager();
+    transactionManager.setEntityManagerFactory(emf);
+    return transactionManager;
+  }
+  
+
+
 
 }
