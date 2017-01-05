412c397390128468f7d043372d86ff37c3a2bc98
Jean Bertrand <jeanbertrand.contact@gmail.com>
2017-01-05 22:58:35 +0100

    Try fucking somthing FUCK

--- src/main/java/fr/eservices/ifi/user_manager/dao/UserDAOImpl.java
+++ src/main/java/fr/eservices/ifi/user_manager/dao/UserDAOImpl.java
@@ -8,8 +8,10 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.Entity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.eservices.ifi.user_manager.entity.User;
 
@@ -34,11 +36,15 @@ public class UserDAOImpl implements UserDAO {
 
 	// Shijin
 	// pas encore tester / ?role type Role et String
+	@Transactional
 	public User createUser(String firstname, String lastname, String email, String password, String role) {
-		TypedQuery<User> query = em.createQuery(
-				"INSERT INTO User VALUES(:firstname, :lastname, :email, :password, :role)", User.class);
+//		TypedQuery<User> query = em.createQuery(
+//				"INSERT INTO User VALUES(:firstname, :lastname, :email, :password, :role)", User.class);
+		
+		Query query = em.createNativeQuery("INSERT INTO User(firstname, lastname, email, password, role) VALUES(:firstname, :lastname, :email, :password, :role)", User.class);
 		query.setParameter("firstname", firstname).setParameter("lastname", lastname).setParameter("email", email).setParameter("password", password).setParameter("role", role);
-		return query.getResultList().get(0);
+		query.executeUpdate();
+		return new User();
 	}
 
 	// Shijin
